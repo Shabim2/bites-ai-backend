9c40afcc8d3f275fd802a72e933c3037545f7c66
Shabi <Shabi.mustafa@gmail.com>
2023-08-03 15:19:53 -0400

    fixed api

--- handlers/parse/getActiveEnergyBurned.py
+++ handlers/parse/getActiveEnergyBurned.py
@@ -2,6 +2,7 @@ import pandas as pd
 import boto3
 import io
 from datetime import datetime, timedelta
+import json
 
 
 def handler(event, context):
@@ -17,25 +18,36 @@ def handler(event, context):
     # Load CSV content into a pandas DataFrame
     df = pd.read_csv(io.StringIO(csv_content))
 
-    # Convert 'endtime' column to datetime
-    df['endtime'] = pd.to_datetime(df['endtime'])
+    # Convert 'endtime' column to datetime with timezone (assuming it's in UTC)
+    df['endtime'] = pd.to_datetime(df['endtime']).dt.tz_convert('UTC')
 
-    # Filter data for the last 30 days
-    last_30_days = datetime.now() - timedelta(days=30)
+    # Find the latest date in the data
+    latest_date = df['endtime'].max()
+
+    # Calculate the date 30 days ago from the latest date in UTC timezone
+    last_30_days = latest_date - timedelta(days=30)
+
+    # Filter data for the latest 30 days
     df = df[df['endtime'] >= last_30_days]
 
     # Extract the date from 'endtime' and group by date to calculate the total calories spent per day
     df['date'] = df['endtime'].dt.date
     result = df.groupby('date')['value'].sum().reset_index()
 
-    # Create the output CSV content
-    output_csv = result.to_csv(index=False)
+    # Convert the 'date' values to strings to make them JSON serializable
+    result['date'] = result['date'].astype(str)
+
+    # Convert the DataFrame to an array of dictionaries (list of objects)
+    data_array = result.to_dict(orient='records')
+
 
     return {
         "statusCode": 200,
         "headers": {
-            "Content-Type": "text/csv",
-            "Content-Disposition": "attachment; filename=calories_spent_per_day.csv"
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'OPTIONS, GET'
         },
-        "body": output_csv
+        "body": json.dumps(data_array)
     }
